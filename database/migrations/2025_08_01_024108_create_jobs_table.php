fe41fc2d62856cd218883df5cd1eee9d649c55ec
rquidet <ralphquidet@gmail.com>
2025-08-01 23:41:15 +0200

    create jobs table migration

---
+++ database/migrations/2025_08_01_024108_create_jobs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 120);
+            $table->longText('description');
+            $table->string('department', 50);
+            $table->string('location', 100);
+            $table->string('city', 60);
+            $table->string('country', 60);
+            $table->string('application_email', 255);
+            $table->string('application_url', 500);
+            $table->string('hours_per_week', 20)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
