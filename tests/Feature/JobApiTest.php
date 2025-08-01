e52310b0a08e87ed1b800ab57021420791ee86b2
rquidet <ralphquidet@gmail.com>
2025-08-02 00:40:38 +0200

    add test and refactor test names

--- tests/Feature/JobApiTest.php
+++ tests/Feature/JobApiTest.php
@@ -9,10 +9,22 @@ use App\Models\Job;
 class JobApiTest extends TestCase
 {
     use RefreshDatabase;
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
+
+    public function test_get_jobs_returns_empty_list(): void
+    {
+        Job::factory()->count(0)->create();
+
+        $response = $this->get('/api/jobs');
+
+        $response->assertStatus(200);
+        $response->assertHeader('Content-Type', 'application/json');
+
+        $content = $response->getContent();
+        $this->assertJson($content);
+        $this->assertEmpty(json_decode($content, true));
+    }
+
+    public function test_get_jobs_returns_job_list(): void
     {
         Job::factory()->count(1)->create();
 
