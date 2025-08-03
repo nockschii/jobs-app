9c9d394e33b419e3486a8f335dbfb303306fa4d3
rquidet <ralphquidet@gmail.com>
2025-08-04 01:34:49 +0200

    protect api endpoints with auth

--- routes/api.php
+++ routes/api.php
@@ -6,20 +6,25 @@ use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\SearchController;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
-
+// Public routes (no authentication required)
 Route::get('/jobs', [JobController::class, 'index'])->name('api.jobs');
 Route::get('/jobs/{id}', [JobController::class, 'show'])->name('api.jobs.show');
-Route::post('/jobs', [JobController::class, 'store'])->name('api.jobs.store');
-Route::put('/jobs/{id}', [JobController::class, 'update'])->name('api.jobs.update');
-Route::delete('/jobs/{id}', [JobController::class, 'destroy'])->name('api.jobs.destroy');
-
 Route::get('/companies', [CompanyController::class, 'index'])->name('api.companies');
 Route::get('/companies/{id}', [CompanyController::class, 'show'])->name('api.companies.show');
-Route::post('/companies', [CompanyController::class, 'store'])->name('api.companies.store');
-Route::put('/companies/{id}', [CompanyController::class, 'update'])->name('api.companies.update');
-Route::delete('/companies/{id}', [CompanyController::class, 'destroy'])->name('api.companies.destroy');
-
 Route::get('/search', [SearchController::class, 'search'])->name('api.search');
+
+// Protected routes (authentication required)
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/jobs', [JobController::class, 'store'])->name('api.jobs.store');
+    Route::put('/jobs/{id}', [JobController::class, 'update'])->name('api.jobs.update');
+    Route::delete('/jobs/{id}', [JobController::class, 'destroy'])->name('api.jobs.destroy');
+
+    Route::post('/companies', [CompanyController::class, 'store'])->name('api.companies.store');
+    Route::put('/companies/{id}', [CompanyController::class, 'update'])->name('api.companies.update');
+    Route::delete('/companies/{id}', [CompanyController::class, 'destroy'])->name('api.companies.destroy');
+});
+
