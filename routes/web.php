ba2b132e5eeaab6ab0835cc49ee75b703daee361
rquidet <ralphquidet@gmail.com>
2025-08-05 03:50:43 +0200

    abort 403 on job create when not authenticated

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\JobController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/jobs/create', [HomeController::class, 'create'])->name('jobs.create');
-});
+Route::get('/jobs/create', [HomeController::class, 'create'])
+    ->middleware('auth.sanctum.redirect')
+    ->name('jobs.create');
